60db78902b288076b9ac66eabb2c6dc744d62905
Andrey Kavarma <kavarmaa@ukr.net>
2024-11-06 22:42:37 +0200

    DZ3.1-changed the text

--- src/kavarma/WorkWithGit.java
+++ src/kavarma/WorkWithGit.java
@@ -2,6 +2,6 @@ package kavarma;
 
 public class WorkWithGit {
     public static void main(String[] args) {
-        System.out.println("Shop-online for customers");
+        System.out.println("This is shop-online app");
     }
 }
